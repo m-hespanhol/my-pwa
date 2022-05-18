f026a40cbfb2ebf994d77fcc6451708554814e97
Marcelo Hespanhol <m_hespanhol@fanshaweonline.ca>
2022-05-18 19:13:29 -0400

    Create service worker

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,15 @@
-console.log('Hello, from scripts.');
+
+console.log('From Scripts:', document);
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/service-worker.js', { scope: '/' })
+    .then(function (registration) {
+      console.log('SW Register success:', registration);
+    })
+    .catch(function (error) {
+      console.log('SW Register failed:', error);
+    });
+}
+else {
+  console.log('Service worker is not supported.');
+}
